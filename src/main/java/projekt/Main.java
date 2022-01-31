9207ff5db5f19fa48861a4445f2501ffa5d09fdd
Julius Florstedt <juliusflorstedt@gmail.com>
2022-01-31 17:45:35 +0100

    Added new array with quadratic numbers.

--- src/main/java/projekt/Main.java
+++ src/main/java/projekt/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++) {
+            a[i] = i*i;
+        }
     }
+
+
 
 
 
