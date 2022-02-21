8b24804a7fa90d9f209b19f66bbfff9825154419
Kraegor <99913822+Kraegor@users.noreply.github.com>
2022-02-21 22:02:52 +0100

    Update Main.java

--- src/main/java/projekt/Main.java
+++ src/main/java/projekt/Main.java
@@ -5,11 +5,7 @@ package projekt;
  */
 public class Main {
 
-    /**
-     * Main entry point in executing the program.
-     *
-     * @param args program arguments, currently ignored
-     */
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
